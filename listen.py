60b7c83c29596634c9441f118c3a5a4a3ec51724
pinae <jonny@pinae.net>
2016-09-07 12:19:22 +0200

    Another MAC

--- listen.py
+++ listen.py
@@ -5,7 +5,7 @@ from scapy.all import sniff, ARP
 
 
 def arp_received(packet):
-    if packet[ARP].op == 1 and packet[ARP].psrc == '0.0.0.0' and packet[ARP].hwsrc == '08:11:96:07:b4:ac':
+    if packet[ARP].op == 1 and packet[ARP].psrc == '0.0.0.0' and packet[ARP].hwsrc == '50:f5:da:6f:98:6c':
         print("Button pressed!")
         print(packet[ARP].hwsrc)
 
