e3033ce321d0a47394f459f6526b4b9f380e7305
pinae <jonny@pinae.net>
2016-09-07 14:32:07 +0200

    Use time to ignore double arp packets

--- listen.py
+++ listen.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 from scapy.all import sniff, ARP
+from datetime import datetime, timedelta
+last_press = datetime.now() - timedelta(seconds=10)
 
 
 def arp_received(packet):
     if packet[ARP].op == 1 and packet[ARP].hwdst == '00:00:00:00:00:00':
         if packet[ARP].hwsrc == '50:f5:da:6f:98:6c':  # This is the MAC of the first dash button
-            print("Button pressed!")
+            global last_press
+            now = datetime.now()
+            if last_press + timedelta(seconds=5) <= now:
+                print("Button pressed!")
+                last_press = now
         elif packet[ARP].hwsrc != 'b8:27:eb:17:d5:22':  # If it is not the MAC of the Raspi it could be another button
             print("Unknown Device connecting: " + packet[ARP].hwsrc)
 
