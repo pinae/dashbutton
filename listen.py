9f4558720c24e0b11271afd8ae901f48c2152771
pinae <jonny@pinae.net>
2016-09-07 14:25:25 +0200

    Do not show new device messages if the ARP packet uses the MAC of the Raspi

--- listen.py
+++ listen.py
@@ -5,9 +5,9 @@ from scapy.all import sniff, ARP
 
 def arp_received(packet):
     if packet[ARP].op == 1 and packet[ARP].hwdst == '00:00:00:00:00:00':
-        if packet[ARP].hwsrc == '50:f5:da:6f:98:6c':
+        if packet[ARP].hwsrc == '50:f5:da:6f:98:6c':  # This is the MAC of the first dash button
             print("Button pressed!")
-        else:
+        elif packet[ARP].hwsrc != 'b8:27:eb:17:d5:22':  # If it is not the MAC of the Raspi it could be another button
             print("Unknown Device connecting: " + packet[ARP].hwsrc)
 
 
